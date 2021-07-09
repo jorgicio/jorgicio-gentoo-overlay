jorgicio/jorgicio-gentoo-overlay

commit 092d9ab5cae1f4265bade244e85b47b8da96511d

Adding fixes to ripgrep-all

sys-apps/ripgrep-all/Manifest
@@ -55,7 +55,7 @@ DIST kernel32-sys-0.2.2.crate 24537 BLAKE2B dfc4a1e31d3ec9e2ac62b890839919fb7fed
 DIST lazy_static-1.3.0.crate 10616 BLAKE2B caa2af77f69c96b5f35dbb2aad7e73784544e6baa5e1fcc2272738fcd3d5f0576480c8169cdddc665a953eb9b6b7ddeb69e93cfa60401458afbe783fff82d1b1 SHA512 08288790139876765b6d4a5988f47fd2a4bfc77c2f2406ad44e64920a471b5655c7f54cb197e5a40c29ee8b42aecbbefaac2b6f4a7dd2b5e24dd92c46cb9b822
 DIST libc-0.2.58.crate 409031 BLAKE2B 81353f2e0962bdc3bca806a195c80b84f9c63c0a7184aad966f33ea073982045577eebb468bf379a7e342bac722b9df7052d25becd9f25c99923bc309ae75211 SHA512 9127ad9a94f75655740fc3a2278c7a17d5f03c4cd12c8833c1a1ecb7a860ec8581fbc969f5c2e23b8eecb9131d9d8271131ad6a675b785f18fb55d830bcc0491
 DIST libflate-0.1.23.crate 35964 BLAKE2B 807d04a03cc711d0c203d65c228c5e9cc6d0e7dffe392d469e87fa75b48e67a667f9c78eb90d5a18047a82331236f22d601af93ef8bf3854afb13a137a1682db SHA512 68c085a12c3aad4ced228782fe38658e43ed620734bad9c1e1cc2d418ee70d19b9991c7c1d953014f69e5f97dd2dc1be57432c034b7a00d44bb1cea2ea8a2793
-DIST libsqlite3-sys-0.14.0.crate 2210492 BLAKE2B da3a187a13c9b247daa9c7a6c80d497a8f85bf6244cef9dfb4705cffb654f4caa21c92a421947c28e8aa875fc3c544800f1b563bfbbf81b42de8d6d160cc258e SHA512 4ec4d2a22f5d6706351a0d104389b6d59f0bf9b8849d4d922ccee007d8fc15e09a5b11b821a7a0f2c5784c500601e529f9afb667b7639783a1fca6928ef463e8
+DIST libsqlite3-sys-0.16.0.crate 2229572 BLAKE2B 4f3203062b3b5cdcdb62a84d6db5f69d48bb537564c51862d10fd7d43ae330a453354ebec29f5a1aea6d3b958a4527e324f7c0fba21f049e992518ae8711ca22 SHA512 48c274d1507b9bd12421091fb70cba635fae934ef81ffd69c5b2a72a42c51dac4afd5bfb80d4d3f7f5a3c9ed44497b7da9741dda9160a52fd6d745d280518df0
 DIST linked-hash-map-0.5.2.crate 16109 BLAKE2B e65a12161dd8b4c0ca49d25e20d0e5b68c411c349c00c6a1faa01f4db4b3f4c811bf9b64947922b8ab900a6bcc90db30e8a6d58ba9e384201879556408803e5e SHA512 74861f70ed09b119c6db1b0dc7f7c8c3483c2824955bf88ee892b3a3fa1a54e9bfcaf7fae0cbc4792762e330584dd347532532e1a0e8a8bca222b5cdb2c7cf94
 DIST lmdb-rkv-0.11.4.crate 25624 BLAKE2B 2c836e38f333ba8c11ce6c6f725af5fdd72bca8463d3676c2eedd3168daeeb084b8bca317d2aafc19a071f91afc4681bb7e2c556f2793ede125ca657dc087699 SHA512 d297fc982c06bebf34a5f280cf1b49d4cb33113035632038e1b767fb8da24c9ec6728f68e0477c29eb5c1099d3bd6934f217ab810f3f0753c5812cbd4a145192
 DIST lmdb-rkv-sys-0.8.3.crate 147654 BLAKE2B f27c24863c528aac66f5953bb2ad5dafff50dd948717203c8ac665b97e1682bec95e6a8049ef1427108e3dcd837d7d1181315252be76a683e239bc374203f0c4 SHA512 55d191cca61c5c266ebd350a7fba981f4de019ce37d8c602c951bb81d8d1616226bb82bcf4481ac485132e01b2a0b0a0a30aa493714d9b119e59f887f39ea63d
@@ -112,10 +112,9 @@ DIST redox_termios-0.1.1.crate 3227 BLAKE2B 0b109d34e942735e804dd3da2a9639506e8b
 DIST regex-1.1.6.crate 243429 BLAKE2B a41cf5b9fa83a7fd4e0a18b15d30f9faacc36267ab0546927061323427eabf4b66893a487f5f59043226d04b700e5ccdd77b47df0219562b6a6615453f271ffe SHA512 f2c039ea729f8360f086b83a8f3da4966932d9bf252ebdbd2058d0249afe997c08c2065efb597e88b55266a18fbe44aa61c67f49ec5047dc6a5bff310f04c32e
 DIST regex-syntax-0.6.6.crate 273068 BLAKE2B 283806a4fc1f0656d2984e8d970ef1db515a56ec08573e397fc342e6f61b6534941530a0ba7fbea69d3817a043dc110b11322ec14b50ce59c2976deb6a66e393 SHA512 5b21640438443394631d28ed1ee391e82bf8b2afc0b3cdbf67f33319064ef4771dc250dfc555d7ff0fb453da3b71e30caebb37ca5c0be446cc40726228c51b76
 DIST remove_dir_all-0.5.1.crate 8726 BLAKE2B b654deae9905c03b068f07d17b5fe3715393cf7c495ee017467fe1cdf955bf44781516d0e6aaa1dd738ace3b3bdd4fb38f7edaaab062eeba02baf50a1250ed0f SHA512 9ee9a11717d45aebd28cd2f50719a2183cc623bdb1af099701e4edbdb9c98bc14cf2278ba06aca4658bfeddaffff00d0d1464c82d175fba7d6fb272918dd6928
-DIST ripgrep-all-0.9.2.tar.gz 6433539 BLAKE2B b4301c77e01ef841af211155fb6600cd4f0ee22d36f550701d54d862ff33662b645c4656d94693f3ce2ebe33d1294e4154eb14513a6d44b5214a0662061aa165 SHA512 ea91feae191b0dc6654572c3c7ace08ea452d3e838fdb5c3f6691a32d510f3340aa64171caf4f67db867e027317e04ffccc1aa6edc95c0ec8942d6e4d8ea0208
 DIST ripgrep-all-0.9.3.tar.gz 6433569 BLAKE2B 14bc539b0937b1df30a258d80395a6a43b61148f1527d8156a6b32fab963604800726bb34f78684eae2e0beb19850287fa0853dbfffb5ad9608ce776dd7889fc SHA512 fc898130e9c5c3666e14bb7f7f1cc5c069a614eea2aa58e84e2267ea1cfcbab8a9272b56f997b7418088950fd5f119c9af050677d28c5a2287b7671dee0d629e
 DIST rkv-0.9.6.crate 39034 BLAKE2B a4ca57235097166fb6ddc5eb628fb3e4cfe6c664ddb0391aaa64d7957595a2982925cc524bc9ee54757b346fca25119ab2164d620762b7f850a0e2efed54d2f7 SHA512 5c1b80fb0ec89fb618a33c5aae805d6d0751eeee9ef014cb3365a5a6a13a9dad91a0e11b0e5bb73c4c150755d1c95a8894b9e78cf715047fb054db684ac2ef28
-DIST rusqlite-0.18.0.crate 95462 BLAKE2B 25607f2d55c3f4e81b9f63d4cc2fa8eb7f46692925e7fcd399574776cf8f44a70d43e83042fd03294e947badf23b6bc492d7f6688af7dd026fc540d1cb057bee SHA512 d3d65e5573d5a18f144957cbfd2f0b1f906e34a7d2cb2be4f4536c3ae34f4eeba152e5c27671a02eca5bcbcee529586ca69643c2f5bf18dc6cdce9c82460541d
+DIST rusqlite-0.20.0.crate 98219 BLAKE2B 324cc676dfa1f3f2fb791e5e64907dc38ade9d0e609ba37b8513499faf146e89c9ff0522b9706d0c7a3637d0239e0c7ccfd60964c853b074593c0ee70d3c7117 SHA512 2a2330debf4d956a18ac0e96507c8103d07cd9ec0c89bdedc568b8c55fdc7963372b82da4b6f6c22a396bd8975200ff3265bc75a81b2b8039f0b4138b1e57808
 DIST rustc-demangle-0.1.15.crate 18640 BLAKE2B d2159fb3bbfc6b3576fc20a6e37d4e7a30f2b8c43ec2528c508015323f209a65cc673fbb4c9275140a1c65dfb1679bbc2d37de606095d2c951610e9d2809074d SHA512 9d10aae8df45db86fcee7fd51edc60961221cf2e3e74e826360fd01c8aa4dbf64eb1de4431309e6db89966f8f88c0933a35156835696f4693fc6f9d5c2e5fd1c
 DIST rustc_version-0.2.3.crate 10210 BLAKE2B 1f707d1c66d907c9b1d2ef6d0bc6bc9c89990a7b922a6dfc2041865940ff3854c4d0e1daf558279fd5871af5818d4000457a32c949104f46538312fbf730443b SHA512 01fa7a758dcaa4f15c18628a0d5ee5adde5ade96a8b7dde0908e39e27b290d1e9adb20d44e2adcd379341d2d4a0c34a80dc12553a3bb4efa4758988f28989779
 DIST ryu-0.2.8.crate 42798 BLAKE2B 393eba320c417af8d2cc23f5fc2874d7813fc3cd4804f7cba1d374f073f74bda4de8208544f30a5650d21cccad28611c8ab2bbdce15ba63b88d6b17100ac5ea7 SHA512 15a17fa06cb971847386013b7bc80e0483bb30f62062ac1e3200d588cb52771a7d34cdd74aba51de46341d303bd29065cf1f8cdcc17c23576cfadaefe63384b4
@@ -167,7 +166,6 @@ DIST zip-0.5.2.crate 29522 BLAKE2B e86f9036b0e68be7e1a95a9a4d461fc4fefd89445d67d
 DIST zstd-0.4.24+zstd.1.4.0.crate 20624 BLAKE2B e4249a793827b7e4ea79ec4eca23f8c79b60fac82788d37990b8e9afa91f3caedd9ea29d0084e0af127d98801be7fa92c6896b389b02396d28b009e91e9407d0 SHA512 5455b954ef1c2954658f3f28227856dcbd950937656491feb1aaab46738354682a982377e76c6276626e5ef16262cf1e527c1f6f8963ab3c28a3fb10c26ee68c
 DIST zstd-safe-1.4.9+zstd.1.4.0.crate 10798 BLAKE2B ef2250dbc3b28eae71d69fea9fac5c29274e1b88290c0f8ab6bad87366ae18d82dac4d8aa37b50375f4421f4b7a784b97cd9aec4f4e7a046f14250ebc6f25c73 SHA512 f71ea6e43c2b1a37d8ad4efa0b1049b1d2e6c2dd49585ecfc31615d0cc598f8e524cad2a2159814187c6ef5227b1afedfc6a932c835cfd95aaf6bfa7daf05b5c
 DIST zstd-sys-1.4.10+zstd.1.4.0.crate 1946491 BLAKE2B a3e829cc815f583b312b8c3f76e181b27403d9c36433cd77707a7ee684e1e531195e4dfbbe889781059cafa42047dffec972a7d11eacefa939df06eb23ed60be SHA512 89bb85d09c5e82f02b995a7e608ae7744593f4c198d0617d2d58e5bf6f48b52c714e9bda1f50231cc67d8ac250f7c883e5c60f929e810396aab132dea2d14e4d
-EBUILD ripgrep-all-0.9.2.ebuild 3520 BLAKE2B e8b884da55fb5326e725c8dfb2fad71465691248de18132a0e1b732768cdd3e88e03c3682d0ae0ae82befa8c011cda42f2862dcdb20d77c2191cc8a4a54230b4 SHA512 675392a0134741e5db93849686722efff9f483541066d375e521f4379df6f223d7e317952f4c33b71f5693177931999311c17f62aab5c4aa6366f0b9395c9ccc
-EBUILD ripgrep-all-0.9.3.ebuild 3520 BLAKE2B e8b884da55fb5326e725c8dfb2fad71465691248de18132a0e1b732768cdd3e88e03c3682d0ae0ae82befa8c011cda42f2862dcdb20d77c2191cc8a4a54230b4 SHA512 675392a0134741e5db93849686722efff9f483541066d375e521f4379df6f223d7e317952f4c33b71f5693177931999311c17f62aab5c4aa6366f0b9395c9ccc
-EBUILD ripgrep-all-9999.ebuild 3466 BLAKE2B 878917cc90991ad0ec044731dfa309d431acb2287e1d9f12d968ed03ba1c0a99c2847b39be413a6cfcbdcb1200586386435ce44dea75b911d34ef9ec1ac9d8a6 SHA512 e7dde7620f36d87ccbe9223c0e3f668c2be028f737be43d7c3bb355dd6d09bdbf2a3376d5d2ce7d2f49b170143156241085286861cd0c39304775afd7f228a36
+EBUILD ripgrep-all-0.9.3.ebuild 3519 BLAKE2B 0f079799e13ea13f7b9ed98ab36dfa721872a5cd21bf6289803bb2297cecb0f3549a407a00621d1fc8a678e9882d4373fdbbc05984cd6570f51d4fb2a8ad5daa SHA512 a8b19239c61c0fbaf6bb7516398c54c89ad805c838317dea3e97e27ebc521a44dd9ba17cd86520032c31e2de2471a05de947e5ebdeee1896701ddb9c35dc0e66
+EBUILD ripgrep-all-9999.ebuild 691 BLAKE2B c7b47579c6aa8bc1b834449c810ee6c0f7d80f681cc2a7805d5e510f925385874348983a29e6f88525207393073af1f3b09b8584f9a7cb7269a70fac4d3d50fc SHA512 5b1d3e9fc90c610fe78a3720f27fa3b2ddd31448de13831d28115a8486eb71ca7ec493a4489160decd8910213fbc0f2be117851fe1caf4dd68680e4ccea4e185
 MISC metadata.xml 317 BLAKE2B a064dab932a673a4ccd23732ebc842cd57c3b9faca938fe291ad7286b5649ff5f3cd8ac1191b340e621a20f3888dcab3470ec041aae115e3cba0a9eab7b67068 SHA512 2b7cae78785fa166c52e6d3eba008543924b3b057ffce125c9afce60453dbde9e8b65546042de78bd48cf4697d6487213a281e7ccad4ac44a944b41d89f2387b

sys-apps/ripgrep-all/ripgrep-all-9999.ebuild
@@ -3,177 +3,9 @@
 
 EAPI=7
 
-CRATES="
-adler32-1.0.3
-aho-corasick-0.7.3
-ansi_term-0.11.0
-arrayref-0.3.5
-arrayvec-0.4.10
-atty-0.2.11
-autocfg-0.1.4
-backtrace-0.3.30
-backtrace-sys-0.1.28
-bincode-1.1.4
-bitflags-1.1.0
-build_const-0.2.1
-byteorder-1.3.1
-bzip2-0.3.3
-bzip2-sys-0.1.7
-cachedir-0.1.1
-cc-1.0.37
-cfg-if-0.1.9
-chrono-0.4.6
-clap-2.33.0
-cloudabi-0.0.3
-crc-1.8.1
-crc32fast-1.2.0
-crossbeam-0.7.1
-crossbeam-channel-0.3.8
-crossbeam-deque-0.2.0
-crossbeam-deque-0.7.1
-crossbeam-epoch-0.3.1
-crossbeam-epoch-0.7.1
-crossbeam-queue-0.1.2
-crossbeam-utils-0.2.2
-crossbeam-utils-0.6.5
-either-1.5.2
-encoding_rs-0.8.17
-encoding_rs_io-0.1.6
-env_logger-0.6.1
-exitfailure-0.5.1
-failure-0.1.5
-failure_derive-0.1.5
-fallible-iterator-0.2.0
-fallible-streaming-iterator-0.1.9
-filetime-0.2.6
-fixedbitset-0.1.9
-flate2-1.0.7
-fnv-1.0.6
-fuchsia-cprng-0.1.1
-generic-array-0.12.0
-glob-0.2.11
-glob-0.3.0
-heck-0.3.1
-humantime-1.2.0
-idna-0.1.5
-itoa-0.4.4
-kernel32-sys-0.2.2
-lazy_static-1.3.0
-libc-0.2.58
-libflate-0.1.23
-libsqlite3-sys-0.14.0
-linked-hash-map-0.5.2
-lmdb-rkv-0.11.4
-lmdb-rkv-sys-0.8.3
-lock_api-0.2.0
-log-0.4.6
-lru-cache-0.1.2
-lzma-sys-0.1.14
-matches-0.1.8
-memchr-2.2.0
-memoffset-0.2.1
-miniz-sys-0.1.12
-miniz_oxide-0.2.1
-miniz_oxide_c_api-0.2.1
-nodrop-0.1.13
-nom-2.2.1
-num-0.2.0
-num-complex-0.2.1
-num-integer-0.1.41
-num-iter-0.1.39
-num-rational-0.2.1
-num-traits-0.2.8
-num_cpus-1.10.0
-numtoa-0.1.0
-ordered-float-1.0.2
-ordermap-0.3.5
-parking_lot-0.8.0
-parking_lot_core-0.5.0
-paste-0.1.5
-paste-impl-0.1.5
-path-clean-0.1.0
-percent-encoding-1.0.1
-petgraph-0.4.13
-pkg-config-0.3.14
-podio-0.1.6
-proc-macro-hack-0.5.7
-proc-macro2-0.4.30
-quick-error-1.2.2
-quote-0.6.12
-rand-0.6.5
-rand_chacha-0.1.1
-rand_core-0.3.1
-rand_core-0.4.0
-rand_hc-0.1.0
-rand_isaac-0.1.1
-rand_jitter-0.1.4
-rand_os-0.1.3
-rand_pcg-0.1.2
-rand_xorshift-0.1.1
-rayon-1.0.3
-rayon-core-1.4.1
-rdrand-0.4.0
-redox_syscall-0.1.54
-redox_termios-0.1.1
-regex-1.1.6
-regex-syntax-0.6.6
-remove_dir_all-0.5.1
-rkv-0.9.6
-rusqlite-0.18.0
-rustc-demangle-0.1.15
-rustc_version-0.2.3
-ryu-0.2.8
-scopeguard-0.3.3
-scopeguard-1.0.0
-semver-0.9.0
-semver-parser-0.7.0
-serde-1.0.92
-serde_derive-1.0.92
-serde_json-1.0.39
-size_format-1.0.2
-smallvec-0.6.9
-strsim-0.8.0
-structopt-0.2.17
-structopt-derive-0.2.17
-syn-0.15.34
-synstructure-0.10.2
-tar-0.4.26
-tempfile-3.0.8
-term_size-0.3.1
-termcolor-1.0.5
-termion-1.5.2
-textwrap-0.11.0
-thread_local-0.3.6
-time-0.1.42
-tree_magic_fork-0.2.2
-typenum-1.10.0
-ucd-util-0.1.3
-unicode-bidi-0.3.4
-unicode-normalization-0.1.8
-unicode-segmentation-1.3.0
-unicode-width-0.1.5
-unicode-xid-0.1.0
-url-1.7.2
-utf8-ranges-1.0.2
-uuid-0.7.4
-vcpkg-0.2.6
-vec_map-0.8.1
-winapi-0.2.8
-winapi-0.3.7
-winapi-build-0.1.1
-winapi-i686-pc-windows-gnu-0.4.0
-winapi-util-0.1.2
-winapi-x86_64-pc-windows-gnu-0.4.0
-wincolor-1.0.1
-xattr-0.2.2
-xz2-0.1.6
-zip-0.5.2
-zstd-0.4.24+zstd.1.4.0
-zstd-safe-1.4.9+zstd.1.4.0
-zstd-sys-1.4.10+zstd.1.4.0
-"
-
 inherit cargo git-r3
+
+CARGO_FETCH_CRATES=yes
 
 DESCRIPTION="ripgrep, but also search in PDFs, e-books, office documents, compressed, etc."
 HOMEPAGE="https://github.com/phiresky/ripgrep-all"
@@ -190,6 +22,7 @@ RDEPEND="
 	virtual/ffmpeg
 "
 DEPEND="${RDEPEND}"
+RESTRICT="network-sandbox"
 
 pkg_postinst(){
 	echo

sys-apps/ripgrep-all/ripgrep-all-0.9.2.ebuild
@@ -1,203 +1,0 @@
-# Copyright 2020 Gentoo Authors
-# Distributed under the terms of the GNU General Public License v2
-
-EAPI=7
-
-CRATES="
-adler32-1.0.3
-aho-corasick-0.7.3
-ansi_term-0.11.0
-arrayref-0.3.5
-arrayvec-0.4.10
-atty-0.2.11
-autocfg-0.1.4
-backtrace-0.3.30
-backtrace-sys-0.1.28
-bincode-1.1.4
-bitflags-1.1.0
-build_const-0.2.1
-byteorder-1.3.1
-bzip2-0.3.3
-bzip2-sys-0.1.7
-cachedir-0.1.1
-cc-1.0.37
-cfg-if-0.1.9
-chrono-0.4.6
-clap-2.33.0
-cloudabi-0.0.3
-crc-1.8.1
-crc32fast-1.2.0
-crossbeam-0.7.1
-crossbeam-channel-0.3.8
-crossbeam-deque-0.2.0
-crossbeam-deque-0.7.1
-crossbeam-epoch-0.3.1
-crossbeam-epoch-0.7.1
-crossbeam-queue-0.1.2
-crossbeam-utils-0.2.2
-crossbeam-utils-0.6.5
-either-1.5.2
-encoding_rs-0.8.17
-encoding_rs_io-0.1.6
-env_logger-0.6.1
-exitfailure-0.5.1
-failure-0.1.5
-failure_derive-0.1.5
-fallible-iterator-0.2.0
-fallible-streaming-iterator-0.1.9
-filetime-0.2.6
-fixedbitset-0.1.9
-flate2-1.0.7
-fnv-1.0.6
-fuchsia-cprng-0.1.1
-generic-array-0.12.0
-glob-0.2.11
-glob-0.3.0
-heck-0.3.1
-humantime-1.2.0
-idna-0.1.5
-itoa-0.4.4
-kernel32-sys-0.2.2
-lazy_static-1.3.0
-libc-0.2.58
-libflate-0.1.23
-libsqlite3-sys-0.14.0
-linked-hash-map-0.5.2
-lmdb-rkv-0.11.4
-lmdb-rkv-sys-0.8.3
-lock_api-0.2.0
-log-0.4.6
-lru-cache-0.1.2
-lzma-sys-0.1.14
-matches-0.1.8
-memchr-2.2.0
-memoffset-0.2.1
-miniz-sys-0.1.12
-miniz_oxide-0.2.1
-miniz_oxide_c_api-0.2.1
-nodrop-0.1.13
-nom-2.2.1
-num-0.2.0
-num-complex-0.2.1
-num-integer-0.1.41
-num-iter-0.1.39
-num-rational-0.2.1
-num-traits-0.2.8
-num_cpus-1.10.0
-numtoa-0.1.0
-ordered-float-1.0.2
-ordermap-0.3.5
-parking_lot-0.8.0
-parking_lot_core-0.5.0
-paste-0.1.5
-paste-impl-0.1.5
-path-clean-0.1.0
-percent-encoding-1.0.1
-petgraph-0.4.13
-pkg-config-0.3.14
-podio-0.1.6
-proc-macro-hack-0.5.7
-proc-macro2-0.4.30
-quick-error-1.2.2
-quote-0.6.12
-rand-0.6.5
-rand_chacha-0.1.1
-rand_core-0.3.1
-rand_core-0.4.0
-rand_hc-0.1.0
-rand_isaac-0.1.1
-rand_jitter-0.1.4
-rand_os-0.1.3
-rand_pcg-0.1.2
-rand_xorshift-0.1.1
-rayon-1.0.3
-rayon-core-1.4.1
-rdrand-0.4.0
-redox_syscall-0.1.54
-redox_termios-0.1.1
-regex-1.1.6
-regex-syntax-0.6.6
-remove_dir_all-0.5.1
-rkv-0.9.6
-rusqlite-0.18.0
-rustc-demangle-0.1.15
-rustc_version-0.2.3
-ryu-0.2.8
-scopeguard-0.3.3
-scopeguard-1.0.0
-semver-0.9.0
-semver-parser-0.7.0
-serde-1.0.92
-serde_derive-1.0.92
-serde_json-1.0.39
-size_format-1.0.2
-smallvec-0.6.9
-strsim-0.8.0
-structopt-0.2.17
-structopt-derive-0.2.17
-syn-0.15.34
-synstructure-0.10.2
-tar-0.4.26
-tempfile-3.0.8
-term_size-0.3.1
-termcolor-1.0.5
-termion-1.5.2
-textwrap-0.11.0
-thread_local-0.3.6
-time-0.1.42
-tree_magic_fork-0.2.2
-typenum-1.10.0
-ucd-util-0.1.3
-unicode-bidi-0.3.4
-unicode-normalization-0.1.8
-unicode-segmentation-1.3.0
-unicode-width-0.1.5
-unicode-xid-0.1.0
-url-1.7.2
-utf8-ranges-1.0.2
-uuid-0.7.4
-vcpkg-0.2.6
-vec_map-0.8.1
-winapi-0.2.8
-winapi-0.3.7
-winapi-build-0.1.1
-winapi-i686-pc-windows-gnu-0.4.0
-winapi-util-0.1.2
-winapi-x86_64-pc-windows-gnu-0.4.0
-wincolor-1.0.1
-xattr-0.2.2
-xz2-0.1.6
-zip-0.5.2
-zstd-0.4.24+zstd.1.4.0
-zstd-safe-1.4.9+zstd.1.4.0
-zstd-sys-1.4.10+zstd.1.4.0
-"
-
-inherit cargo
-
-DESCRIPTION="ripgrep, but also search in PDFs, e-books, office documents, compressed, etc."
-HOMEPAGE="https://github.com/phiresky/ripgrep-all"
-SRC_URI="
-	${HOMEPAGE}/archive/${PV}.tar.gz -> ${P}.tar.gz
-	$(cargo_crate_uris ${CRATES})
-"
-
-LICENSE="AGPL-3"
-SLOT="0"
-KEYWORDS="~amd64 ~arm64 ~ppc64 ~x86"
-
-RDEPEND="
-	app-text/pandoc
-	app-text/poppler
-	sys-apps/ripgrep
-	virtual/ffmpeg
-"
-DEPEND="${RDEPEND}"
-
-pkg_postinst(){
-	echo
-	elog "In order to use ripgrep-all, use the rga command."
-	elog "You can also use the preprocessor using the command"
-	elog "rga-preproc."
-	echo
-}

sys-apps/ripgrep-all/ripgrep-all-0.9.3.ebuild
@@ -61,7 +61,7 @@ kernel32-sys-0.2.2
 lazy_static-1.3.0
 libc-0.2.58
 libflate-0.1.23
-libsqlite3-sys-0.14.0
+libsqlite3-sys-0.16.0
 linked-hash-map-0.5.2
 lmdb-rkv-0.11.4
 lmdb-rkv-sys-0.8.3
@@ -119,7 +119,7 @@ regex-1.1.6
 regex-syntax-0.6.6
 remove_dir_all-0.5.1
 rkv-0.9.6
-rusqlite-0.18.0
+rusqlite-0.20.0
 rustc-demangle-0.1.15
 rustc_version-0.2.3
 ryu-0.2.8
@@ -170,8 +170,7 @@ xz2-0.1.6
 zip-0.5.2
 zstd-0.4.24+zstd.1.4.0
 zstd-safe-1.4.9+zstd.1.4.0
-zstd-sys-1.4.10+zstd.1.4.0
-"
+zstd-sys-1.4.10+zstd.1.4.0"
 
 inherit cargo
 

commit e1fe5c57a2fc9308f12bf1966fd6b87640382e59

ripgrep-all: Prevent failure with live ebuild
Live ebuild does not have the list of crates and with new cargo.eclass it can be
fetched live during src_unpack, so fix that.

Fixes: 092d9ab5cae1f4265bade244e85b47b8da96511d

sys-apps/ripgrep-all/Manifest
@@ -284,5 +284,5 @@ DIST zstd-sys-1.4.15+zstd.1.4.4.crate 2014730 BLAKE2B ac978e2103688671f1dccb30da
 EBUILD ripgrep-all-0.9.3-r1.ebuild 3528 BLAKE2B e33c8d972e47ee3c501cf411e0d7471e0b42ee6b984cbd288b11d8570c6056f1a5c8e14400cb361f74391a48fdec64b25dde5feac4339bb80a5f6d8b945cdf56 SHA512 3e0c72a7115c14ee1d43a5514fa0ca202168d7af2615c42d1395292925bf3d0cb5d5b2fc8716bc4a200604c352af148b295f772106a230d0fc01b130ff7a77cf
 EBUILD ripgrep-all-0.9.3.ebuild 3519 BLAKE2B 0f079799e13ea13f7b9ed98ab36dfa721872a5cd21bf6289803bb2297cecb0f3549a407a00621d1fc8a678e9882d4373fdbbc05984cd6570f51d4fb2a8ad5daa SHA512 a8b19239c61c0fbaf6bb7516398c54c89ad805c838317dea3e97e27ebc521a44dd9ba17cd86520032c31e2de2471a05de947e5ebdeee1896701ddb9c35dc0e66
 EBUILD ripgrep-all-0.9.6.ebuild 3525 BLAKE2B 8a0becc8dce6704d83e9baf82b79232d4f401c4471765122aa880680a4dcc337037d5e5a1f5429be793564e855cf0a8becdac042d122f365793624b1cba235e9 SHA512 ff088beac9565f6d6cd3f03da3e452aeaa1bb04e233a216e3a49d6020b6d2df8a3a21851b0c96e201eced3666597fb6f27b5a90791da64ef6480a719daa7ff11
-EBUILD ripgrep-all-9999.ebuild 691 BLAKE2B c7b47579c6aa8bc1b834449c810ee6c0f7d80f681cc2a7805d5e510f925385874348983a29e6f88525207393073af1f3b09b8584f9a7cb7269a70fac4d3d50fc SHA512 5b1d3e9fc90c610fe78a3720f27fa3b2ddd31448de13831d28115a8486eb71ca7ec493a4489160decd8910213fbc0f2be117851fe1caf4dd68680e4ccea4e185
+EBUILD ripgrep-all-9999.ebuild 696 BLAKE2B ba702fc837e289744b30e299d07b7806527a8bae6b743984e487e7fa3ade6dd8b759acdf40c1ce4764d8b3e2f3dd651e478b73398ba006cbae01b9515bc342af SHA512 66cd5dcd80162e62dc4d9d2a27864b308146bfe5b3ffaced859a71d9d5ad38be852152d07cc0cad7b0a2fb3cd67952704d454a42e689af1f5fd32301a80726f1
 MISC metadata.xml 317 BLAKE2B a064dab932a673a4ccd23732ebc842cd57c3b9faca938fe291ad7286b5649ff5f3cd8ac1191b340e621a20f3888dcab3470ec041aae115e3cba0a9eab7b67068 SHA512 2b7cae78785fa166c52e6d3eba008543924b3b057ffce125c9afce60453dbde9e8b65546042de78bd48cf4697d6487213a281e7ccad4ac44a944b41d89f2387b

sys-apps/ripgrep-all/ripgrep-all-9999.ebuild
@@ -5,11 +5,8 @@ EAPI=7
 
 inherit cargo git-r3
 
-CARGO_FETCH_CRATES=yes
-
 DESCRIPTION="ripgrep, but also search in PDFs, e-books, office documents, compressed, etc."
 HOMEPAGE="https://github.com/phiresky/ripgrep-all"
-SRC_URI="$(cargo_crate_uris ${CRATES})"
 EGIT_REPO_URI="${HOMEPAGE}"
 
 LICENSE="AGPL-3"
@@ -19,10 +16,15 @@ RDEPEND="
 	app-text/pandoc
 	app-text/poppler
 	sys-apps/ripgrep
-	virtual/ffmpeg
+	media-video/ffmpeg
 "
 DEPEND="${RDEPEND}"
 RESTRICT="network-sandbox"
+
+src_unpack() {
+	git-r3_src_unpack
+	cargo_live_src_unpack
+}
 
 pkg_postinst(){
 	echo